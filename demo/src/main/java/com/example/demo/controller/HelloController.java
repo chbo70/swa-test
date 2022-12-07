3f19a999edc516cdf21f0381d2570e099f03b2c3
Boon-Chung <boon-chung.chi@student.uibk.ac.at>
2022-12-07 11:25:09 +0100

    second comm

---
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "hello there, hannes";
+    }
+}
